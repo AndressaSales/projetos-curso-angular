e5e792c84bec1d4e1c150de472dbc829501927dd
AndressaSales <andressaccs2001@gmail.com>
2025-07-04 15:20:14 -0300

    adicionando pasta forms

---
+++ forms/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+
+import { TemplateForm } from './components/template-form/template-form';
+import { DataForm } from './components/data-form/data-form';
+
+export const routes: Routes = [
+    {path: 'templateForm', title: 'Template', component: TemplateForm},
+    {path: 'dataForm', title: 'Data', component: DataForm},
+    {path: '', pathMatch: 'full', redirectTo: 'TemplateForm'}
+];
